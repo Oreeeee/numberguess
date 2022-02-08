8437b5df3d633bf5f8670c650fffdc819c8a4147
Oreeeee <lifehackeeer2@gmail.com>
2022-02-08 15:36:04 +0100

    Fixed a bug where if you win the first time the program crashes

--- numberguess.py
+++ numberguess.py
@@ -3,6 +3,7 @@ import random
 hidden_number = random.randrange(10) + 1
 failed_attempts = 0
 usable_attempts = 3
+attempts_left = usable_attempts
 
 print("Rules: You have to guess a random number between 1 and 10, and you have 3 chances. This program will tell you is this a bigger or smaller number.")
 
@@ -12,7 +13,7 @@ while failed_attempts < usable_attempts:
     user_input = int(input("Number: "))
     failed_attempts = failed_attempts + 1
     if user_input == hidden_number:
-        print(f"Congratulations, you won! You had {attempts_left} attempts left.")
+        print(f"Congratulations, you won! You had {attempts_left} attempt(s) left.")
         break
     elif user_input > hidden_number:
         attempts_left = usable_attempts - failed_attempts
